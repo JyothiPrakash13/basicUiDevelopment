1ce9f04dc9fd3523e2bd35c85d2537924eca3436
Jyothi Prakash <jyothi.prakash@talentship.io>
2024-06-10 07:55:38 +0530

    Initial set up

---
+++ style-sculptor/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./custom.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="grid-container">
+          <div className="sidebar">Sidebar</div>
+          <div className="main-content">Main Content</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
